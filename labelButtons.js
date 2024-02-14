15f2cd9b3fb5e7f3d77c7f71c6d758e6debba54a
AERobert <egglestonarobert@gmail.com>
2024-02-14 06:54:00 -0500

    added basic functionality to alert the user when starting and finishing responding

--- labelButtons.js
+++ labelButtons.js
@@ -1,4 +1,4 @@
-unlabeledButtonIcons = [
+const unlabeledButtonIcons = [
   {
     "name": "regenerate",
     "svg": "M4.5 2.5C5.05228 2.5 5.5 2.94772 5.5 3.5V5.07196C7.19872 3.47759 9.48483 2.5 12 2.5C17.2467 2.5 21.5 6.75329 21.5 12C21.5 17.2467 17.2467 21.5 12 21.5C7.1307 21.5 3.11828 17.8375 2.565 13.1164C2.50071 12.5679 2.89327 12.0711 3.4418 12.0068C3.99033 11.9425 4.48712 12.3351 4.5514 12.8836C4.98798 16.6089 8.15708 19.5 12 19.5C16.1421 19.5 19.5 16.1421 19.5 12C19.5 7.85786 16.1421 4.5 12 4.5C9.7796 4.5 7.7836 5.46469 6.40954 7H9C9.55228 7 10 7.44772 10 8C10 8.55228 9.55228 9 9 9H4.5C3.96064 9 3.52101 8.57299 3.50073 8.03859C3.49983 8.01771 3.49958 7.99677 3.5 7.9758V3.5C3.5 2.94772 3.94771 2.5 4.5 2.5Z"
@@ -59,6 +59,39 @@ unlabeledButtonIcons = [
 
 
 ]
+
+// set up basic announcement system
+
+// create invisible div to contain accessibility announcements
+const accessibilityAnnouncementsDiv = document.createElement('div');
+
+// adds aria attributes to make div useful
+accessibilityAnnouncementsDiv.setAttribute('role', 'alert');
+accessibilityAnnouncementsDiv.id = 'accessibility-announcements';
+
+// needs to be styled to be invisible
+
+accessibilityAnnouncementsDiv.style.position = 'absolute';
+accessibilityAnnouncementsDiv.style.width = '1px';
+accessibilityAnnouncementsDiv.style.height = '1px';
+accessibilityAnnouncementsDiv.style.margin = '-1px';
+accessibilityAnnouncementsDiv.style.padding = '0';
+accessibilityAnnouncementsDiv.style.border = '0';
+accessibilityAnnouncementsDiv.style.clip = 'rect(0 0 0 0)';
+accessibilityAnnouncementsDiv.style.overflow = 'hidden';
+accessibilityAnnouncementsDiv.style.whiteSpace = 'nowrap';
+accessibilityAnnouncementsDiv.style.border = '0';
+
+document.body.appendChild(accessibilityAnnouncementsDiv);
+
+// function to use this div to announce messages to screen reader users
+function announceMessage(message) {
+  accessibilityAnnouncementsDiv.textContent = message;
+  setTimeout(() => {
+    accessibilityAnnouncementsDiv.textContent = '';
+    }, 1000);
+  console.log(`Just announced '${message}'`);
+}
 
 // labeling
 // Assuming unlabeledButtonIcons is already declared and initialized
@@ -135,7 +168,7 @@ const observer = new MutationObserver(mutations => {
             // Perform any action you need when the button is added
           } else if (node.querySelector('button[aria-label="Stop generating"]')) {
             console.log('The "Stop generating" button has been added within a newly added node.');
-            // Perform any action you need when the button is added within a new node
+            announceMessage('responding ...');
           }
         }
       });
@@ -148,7 +181,7 @@ const observer = new MutationObserver(mutations => {
             // Perform any action you need when the button is removed
           } else if (node.querySelector('button[aria-label="Stop generating"]')) {
             console.log('The "Stop generating" button has been removed from a node.');
-            // Perform any action you need when the button is removed from a node
+            announceMessage('finished responding');
           }
         }
       });
